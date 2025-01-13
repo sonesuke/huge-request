b7a539dba5d5ab4d810f8d6aa2e9bb30aa9ddd96
sonesuke <iamsonesuke@gmail.com>
2025-01-13 13:52:44 +0900

    Read parqeut files from the fronend by using parquet-wasm.
    - Use `vite-plugin-wasm`, `vite-plugin-top-level-await` for load wasm in Vite
    - Use `arrow-js-ffi` for zero-copy reading.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+// import * as arrow from "apache-arrow";
+import { parseTable } from "arrow-js-ffi";
+import { DataType } from "apache-arrow";
+import { readParquet, wasmMemory } from "parquet-wasm";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const readHandler = async () => {
+    const WASM_MEMORY = wasmMemory();
+    const data = await fetch("./test.parquet");
+    const buffer = new Uint8Array(await data.arrayBuffer());
+    const arrowWasmTable = readParquet(buffer);
+    const ffiTable = arrowWasmTable.intoFFI();
+    const arrowTable = parseTable(
+      WASM_MEMORY.buffer,
+      ffiTable.arrayAddrs(),
+      ffiTable.schemaAddr()
+    );
+    const fields = arrowTable.schema.fields;
+    const dataArray = arrowTable.toArray();
+
+    fields.forEach((field) => {
+      console.log(field.name);
+      dataArray.forEach((data) => {
+        const val = data[field.name];
+        if (DataType.isFloat(val) || DataType.isInt(val)) {
+          console.log(Number(val));
+        } else {
+          console.log(val.toString());
+        }
+      });
+    });
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <button onClick={readHandler}>Read Parquet</button>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -29,7 +58,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
